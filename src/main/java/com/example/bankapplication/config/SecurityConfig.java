08348cddc0edccb1e619627407fde6dd555f1b7e
Tony Padmore <emperialace@gmail.com>
2023-08-14 19:31:01 -0400

    form login 1.

--- src/main/java/com/example/bankapplication/config/SecurityConfig.java
+++ src/main/java/com/example/bankapplication/config/SecurityConfig.java
@@ -1,22 +1,36 @@
 package com.example.bankapplication.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(HttpMethod.GET, "/login").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/login").permitAll()
                 )
-                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home")
+                        .loginProcessingUrl("login"))
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/example/bankapplication/repository/UserRepository.java
+++ src/main/java/com/example/bankapplication/repository/UserRepository.java
@@ -8,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long>{
 
     User findById(Integer id);
+
+    User findByUserNameAndPassword(String name, String password);
+
 }
